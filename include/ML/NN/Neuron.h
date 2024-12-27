db26db31f2101e52ec0520da1e86253ee646913d
malikkurtz <malikkurtz129@gmail.com>
2024-12-26 22:03:19 -0500

    refactored neuron class

--- include/ML/NN/Neuron.h
+++ include/ML/NN/Neuron.h
@@ -5,6 +5,15 @@
 #include <random>
 #include "NeuronInitializationType.h"
 
+// InputNeuron Class
+class InputNeuron {
+public:
+    // Default constructor
+    InputNeuron();
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // Neuron Class
 class Neuron {
 public:
@@ -12,7 +21,7 @@ public:
     std::vector<double> weights;
 
     // Constructor to initialize weights to a default value
-    explicit Neuron(int num_weights, NeuronInitializationType type);
+    explicit Neuron(int num_weights, NeuronInitializationType initialization_type);
 
     // Getter for weights
     std::vector<double> getWeights() const;
@@ -20,20 +29,8 @@ public:
     // Setter for weights
     void setWeights(const std::vector<double>& new_weights);
 
-    void reInitializeWeights(NeuronInitializationType type);
+    void reInitializeWeights(NeuronInitializationType initialization_type);
 };
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-// InputNeuron Class
-class InputNeuron {
-public:
-
-    // Default constructor
-    InputNeuron();
-
-};
 
 #endif // NEURON_H
--- src/ML/NN/Neuron.cpp
+++ src/ML/NN/Neuron.cpp
@@ -5,16 +5,15 @@
 InputNeuron::InputNeuron() {}
 
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // Neuron Definitions
-Neuron::Neuron(int num_weights, NeuronInitializationType type) {
-    if (type == RANDOM) {
+Neuron::Neuron(int num_weights, NeuronInitializationType initialization_type) {
+    if (initialization_type == RANDOM) {
         weights.resize(num_weights, static_cast<double>(rand()) / RAND_MAX - 0.5); // 
     }
-    else if (type == CONSTANT) {
+    else if (initialization_type == CONSTANT) {
         weights.resize(num_weights, 2);
     } else {
         throw std::invalid_argument("Neuron Initialization not specified!");
@@ -29,12 +28,12 @@ void Neuron::setWeights(const std::vector<double>& new_weights) {
     weights = new_weights;
 }
 
-void Neuron::reInitializeWeights(NeuronInitializationType type)
+void Neuron::reInitializeWeights(NeuronInitializationType initialization_type)
 {
-    if (type == RANDOM) {
+    if (initialization_type == RANDOM) {
         weights.resize(weights.size(), static_cast<double>(rand()) / RAND_MAX - 0.5); // 
     }
-    else if (type == CONSTANT) {
+    else if (initialization_type == CONSTANT) {
         weights.resize(weights.size(), 2);
     } else {
         throw std::invalid_argument("Neuron Initialization not specified!");
