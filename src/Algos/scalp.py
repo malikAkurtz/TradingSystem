7ab3b32c0263e96e534dd51786eacae277a7a465
malikkurtz <malikkurtz129@gmail.com>
2025-01-29 15:11:50 -0500

    working

--- src/Algos/scalp.py
+++ src/Algos/scalp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import pandas as pd
 from alpaca.data import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
@@ -38,11 +39,20 @@ def calculate_SMA(prices, period):
     return (sum(prices) / len(prices))
 
 def calculate_STD(prices, sma_value):
-    for ()
+    running_sum = 0
+    for price in prices:
+        running_sum += (price - sma_value)*2
 
-def calculate_BB(prices, sma_value, standard_deviations = 2):
-    upper_BB = sma_value + standard_deviations * ()
-    return
+    running_sum /= len(prices)
+
+    return math.sqrt(running_sum)
+
+
+def calculate_BB(prices, standard_deviations = 2, period=20):
+    sma_value = calculate_SMA(prices, period)
+    upper_BB = sma_value + (standard_deviations * calculate_STD(prices, sma_value))
+    lower_BB = sma_value - (standard_deviations * calculate_STD(prices, sma_value))
+    return [upper_BB, lower_BB]
 
 
 
@@ -50,13 +60,13 @@ def strat():
     traded_today = False
     # if were in the window in which we can open trades
     if (within_trade_window(trading_client.get_clock())):
-        
+        bars = data_client.get_stock_bars(request_params)
+
+        candles = pd.DataFrame(bars[symbol])
         # if we havent already opened a position
         if (not trading_client.get_all_positions()):
 
-            bars = data_client.get_stock_bars(request_params)
-
-            candles = pd.DataFrame(bars["SPY"])
+            
 
 def main():
     bars = data_client.get_stock_bars(request_params)
