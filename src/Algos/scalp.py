2f0c725d7477ff31decd4489d2c97ead9a618918
malikkurtz <malikkurtz129@gmail.com>
2025-01-29 00:02:58 -0500

    working

--- src/Algos/scalp.py
+++ src/Algos/scalp.py
@@ -1,8 +1,28 @@
+import numpy as np
+import pandas as pd
+from alpaca.data import StockHistoricalDataClient
+from alpaca.data.requests import StockBarsRequest
+from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
+import datetime
+
 from config import API_KEY, API_SECRET, BASE_URL
 from alpaca.trading.client import TradingClient
 
 trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
 
-def main():
-    
+data_client = StockHistoricalDataClient(API_KEY, API_SECRET)
 
+symbol = "SPY"
+
+request_params = StockBarsRequest(symbol_or_symbols="SPY",
+                                  timeframe = TimeFrame(3, TimeFrameUnit.Minute),
+                                  start=datetime.datetime.utcnow() - datetime.timedelta(days=1))
+
+bars = data_client.get_stock_bars(request_params)
+
+df = pd.DataFrame(bars["SPY"])
+
+def main():
+    print(df[3])
+
+main()
