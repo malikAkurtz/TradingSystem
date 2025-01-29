850207328320b035049a00ef145b291e6b793a20
malikkurtz <malikkurtz129@gmail.com>
2025-01-29 13:23:49 -0500

    working

--- src/Algos/scalp.py
+++ src/Algos/scalp.py
@@ -3,7 +3,8 @@ import pandas as pd
 from alpaca.data import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
-import datetime
+from datetime import datetime, time, timedelta
+import pytz
 
 from config import API_KEY, API_SECRET, BASE_URL
 from alpaca.trading.client import TradingClient
@@ -13,16 +14,54 @@ trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
 data_client = StockHistoricalDataClient(API_KEY, API_SECRET)
 
 symbol = "SPY"
-
 request_params = StockBarsRequest(symbol_or_symbols="SPY",
-                                  timeframe = TimeFrame(3, TimeFrameUnit.Minute),
-                                  start=datetime.datetime.utcnow() - datetime.timedelta(days=1))
+                                            timeframe = TimeFrame(3, TimeFrameUnit.Minute),
+                                            start=datetime.utcnow() - timedelta(days=1))
 
-bars = data_client.get_stock_bars(request_params)
+def within_trade_window(input_time):
+    est = pytz.timezone("US/Eastern")
 
-df = pd.DataFrame(bars["SPY"])
+    start_time = datetime.time(12, 30)
+    end_time = datetime.time(2, 30)
+
+    input_time_est = input_time.astimezone(est)
+
+    return start_time <= input_time_est <= end_time
+
+class Order:
+    def __init__(self, side, quantity, price):
+        self.side = side
+        self.quantity = quantity
+        self.price = price
+
+def calculate_SMA(prices, period):
+    return (sum(prices) / len(prices))
+
+def calculate_STD(prices, sma_value):
+    for ()
+
+def calculate_BB(prices, sma_value, standard_deviations = 2):
+    upper_BB = sma_value + standard_deviations * ()
+    return
+
+
+
+def strat():
+    traded_today = False
+    # if were in the window in which we can open trades
+    if (within_trade_window(trading_client.get_clock())):
+        
+        # if we havent already opened a position
+        if (not trading_client.get_all_positions()):
+
+            bars = data_client.get_stock_bars(request_params)
+
+            candles = pd.DataFrame(bars["SPY"])
 
 def main():
-    print(df[3])
+    bars = data_client.get_stock_bars(request_params)
+    candles = pd.DataFrame(bars["SPY"])
+
+    return print(candles)
 
 main()
